b95f215dabe516021d7b8f24a8da000449c7a76a
Kylee Tilley <kyleetilley@gmail.com>
2020-03-12 02:17:42 -0500

    Remove photo test id

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Photo({ id, title, url, thumbnailUrl }) {
   return (
-    <div data-testid={`photo-${id}`}>
+    <div>
       <a href={url} target="_blank" rel="noreferrer noopener">
         <img src={thumbnailUrl} alt={title} />
       </a>
