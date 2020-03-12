8a6b0e322284e836be0abb11dd31e462b6b755d5
Kylee Tilley <kyleetilley@gmail.com>
2020-03-12 02:27:47 -0500

    Add thumbnail size to photos

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,7 +4,11 @@ export default function Photo({ id, title, url, thumbnailUrl }) {
   return (
     <div>
       <a href={url} target="_blank" rel="noreferrer noopener">
-        <img src={thumbnailUrl} alt={title} />
+        <img
+          src={thumbnailUrl}
+          alt={title}
+          style={{ width: "150px", height: "150px" }}
+        />
       </a>
     </div>
   );
